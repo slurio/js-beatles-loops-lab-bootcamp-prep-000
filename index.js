90aacd054d2fc1e8a695dded271ef694800827f9
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-13 03:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicans, instruments) {
   var array = [];
-  for(let i=0, i < musicans.length, i++) {
+  for(let i=0; i < musicans.length; i++) {
     array.push(`${musicans[i]} plays ${instruments[i]}`);
   }
   return array;
